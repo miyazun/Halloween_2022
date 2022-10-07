8b57547f274f18a60fd100c6de0f557c6f82fbe6
miyazun <g2054795@gmail.com>
2022-10-08 03:40:13 +0900

    左右移動の際、体を移動方向に向けるようにしました。

--- Assets/Scripts/CharacterMove.cs
+++ Assets/Scripts/CharacterMove.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UniRx;
 
@@ -9,19 +7,25 @@ public class CharacterMove : MonoBehaviour
     private float speed;
     [SerializeField]
     private Rigidbody2D rb;
+    private float playerSize;
 
     void Start()
     {
+        playerSize = 0.2f;
         rb = GetComponent<Rigidbody2D>();
         var move = Observable.EveryUpdate()
                    .Subscribe(_ => XMove());
     }
-
 
     private void XMove()
     {
         float xForth = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(xForth * speed, rb.velocity.y);
         Debug.Log(rb.velocity);
+
+        if (xForth != 0.0f)
+        {
+            transform.localScale = new Vector2(xForth * playerSize, playerSize);
+        }
     }
 }
